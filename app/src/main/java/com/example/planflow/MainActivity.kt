bf3be5e43b3a0e5def60deddb2b3a4069e884cdf
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2026-02-07 23:53:45 -0300

    Project setup

---
+++ app/src/main/java/com/example/planflow/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.planflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.planflow.ui.theme.PlanFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlanFlowTheme {
+
+            }
+        }
+    }
+}
